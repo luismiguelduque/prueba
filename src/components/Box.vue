096c828cb3f67db6a748629acb9a735d0b3b5105
Luis Duque <luisduque2302@gmail.com>
2020-07-23 01:16:44 -0400

    adding elements in boxes

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,12 +1,40 @@
 <template>
-    <div>
-
+    <div class="columns boxes" :class="(index % 2) ? 'align-inverse':''">
+        <div class="column image-box">
+            <img :src="getImgUrl(data.image)" :alt="data.title">
+        </div>
+        <div class="column info-box">
+            <img :src="'../assets/' + data.imagenIcon" :alt="data.title">
+            <div class="title">
+                {{data.title}}
+            </div>
+            <div class="description">
+                {{data.description}}
+            </div>
+            <div class="buttons">
+                <a class="button is-link" href="#">{{data.link}}</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 
     export default {
-
+        props: {
+            data: Object,
+            index: Number,
+        },
+        methods: {
+            getImgUrl(image){
+                var images = require.context('../assets/', false, /\.jpg$/);
+                return images('./' + image + ".jpg");
+            },
+        },
     }
 </script>
+<style lang="scss">
+    .align-inverse{
+        flex-direction:row-reverse;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,11 @@
   <div>
     <menu-content></menu-content>
     <carrousel-slide></carrousel-slide>
-    <div>
-
-    </div>
+    <box v-for="(product, index) in products"
+         :key="index"
+         :data="product"
+         :index="index">
+    </box>
     <instagram></instagram>
     <suscribite></suscribite>
     <footer-content></footer-content>
@@ -13,17 +15,27 @@
 
 <script>
 // @ is an alias to /src
-//import Box from "../components/Box";
+import Box from "../components/Box";
 import CarrouselSlide from "../components/CarrouselSlide";
 import Instagram from "../components/Instagram";
 import Suscribite from "../components/Suscribite";
 import FooterContent from "../components/FooterContent";
 import MenuContent from "../components/MenuContent";
+import productItems from "../utils/productsItems";
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      products: [],
+    }
+  },
+  mounted() {
+    this.products = productItems;
+    console.log(this.products);
+  },
   components: {
-    //Box,
+    Box,
     FooterContent,
     CarrouselSlide,
     Instagram,
