c1f9eaf00650aea977d7dff5689155f16e140e22
Luis Duque <luisduque2302@gmail.com>
2020-07-22 22:24:29 -0400

    created structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <button class="button is-primary">Primary</button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <menu-content></menu-content>
+    <slider></slider>
+    <div>
+
+    </div>
+    <instagram></instagram>
+    <suscribite></suscribite>
+    <footer-content></footer-content>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import Box from "../components/Box";
+import Slider from "../components/Slider";
+import Instagram from "../components/Instagram";
+import Suscribite from "../components/Suscribite";
+import FooterContent from "../components/FooterContent";
+import MenuContent from "../components/MenuContent";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    //Box,
+    FooterContent,
+    Slider,
+    Instagram,
+    Suscribite,
+    MenuContent,
   }
 }
 </script>
